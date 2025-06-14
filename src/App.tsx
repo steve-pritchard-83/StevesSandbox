48e5defa6e88c6f7d105b04428b631c0d844abb5
steve-pritchard-83 <stevep199@gmail.com>
2025-06-14 14:28:01 +0930

    feat: Create Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import './App.css'
+import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      <h1>Welcome to Your React Sandbox!</h1>
+      <Header />
+      <h2>Welcome to Your React Sandbox!</h2>
       <p>This is your starting point. Let's build something great.</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
