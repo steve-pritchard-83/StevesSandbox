32075cac138416ed9bf2082b4bdf9b2fec11a361
steve-pritchard-83 <stevep199@gmail.com>
2025-06-14 12:51:13 +0930

    feat: Initialize React + TypeScript sandbox with Vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Your React Sandbox!</h1>
+      <p>This is your starting point. Let's build something great.</p>
+    </div>
+  )
+}
+
+export default App
